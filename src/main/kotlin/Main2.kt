8cd76dc9f5b28876474dda1c236d8ba9993f3ff0
Touraj Ebrahimi <toraj58@gmail.com>
2024-03-12 20:51:51 +0100

    let function

--- src/main/kotlin/Main2.kt
+++ src/main/kotlin/Main2.kt
@@ -24,5 +24,21 @@ package org.example
             .surname("Spector").print()
 
 
+        val stringBuilder = StringBuilder()
+        val numberOfCharacters = stringBuilder.let {
+            it.append("This is a transformation function.")
+            it.append("It takes a StringBuilder instance and returns the number of characters in the generated String")
+            it.length
+        }
+
+        println("numberOfCharacters = $numberOfCharacters")
+
+        val message: String? = "hello there!"
+
+        val charactersInMessage = message?.let {
+            "value was not null: $it"
+        } ?: "value was null"
+
+        println(charactersInMessage)
 
     }
